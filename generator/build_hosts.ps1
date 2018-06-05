6b856159173d28343a31000a4b4149bc2a8b8132
MMotti <matthew.w.motti@gmail.com>
2018-06-05 15:24:16 +0100

    Upload host file generator
    Still needs some error handling implemented, but it works.

---
+++ generator/build_hosts.ps1
@@ -0,0 +1,116 @@
+﻿Clear-Host
+
+# Include functions file
+
+. "$PSScriptRoot\includes\scripts\functions.ps1"
+
+# Reset hosts array
+
+$hosts = @()
+
+# User Variables
+
+$parent_dir       = Split-Path $PSScriptRoot
+
+$out_file         = "$parent_dir\hosts"
+
+$web_host_files   = 'http://someonewhocares.org/hosts/hosts',`
+                    'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0', `
+                    'https://zeustracker.abuse.ch/blocklist.php?download=domainblocklist',`
+                    'https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt',`
+                    'https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt'
+
+$local_host_dir   = "$PSScriptRoot\includes\hosts"
+$local_host_files = "$PSScriptRoot\includes\config\blacklist.txt"
+$local_wildcards  = "$PSScriptRoot\includes\config\wildcards.txt"
+$local_regex      = "$PSScriptRoot\includes\config\regex.txt"
+$local_whitelist  = "$PSScriptRoot\includes\config\whitelist.txt"
+$local_nxhosts    = "$PSScriptRoot\includes\config\nxdomains.txt"
+
+# Check the domain is still alive?
+# This can take some time depending on host counts.
+
+$check_heartbeat  = $false
+
+# Fetch Hosts
+
+Write-Output "--> Fetching Hosts"
+
+$hosts            = Fetch-Hosts -w_host_files $web_host_files `
+                                -l_host_files $local_host_files `
+                                -dir $local_host_dir
+
+# Quit in the event of no hosts detected
+
+if(!($hosts))
+{
+    Write-Output "No hosts detected. Please check your configuration."
+    Start-Sleep -Seconds 5
+    exit
+}
+
+# Fetch Wildcards
+
+Write-Output "--> Fetching Wildcards"
+
+$wildcards         = (Get-Content $local_wildcards) -split '\n' | Where {$_}
+
+# Fetch Whitelist
+
+$whitelist         = (Get-Content $local_whitelist) -split '\n' | Where {$_}
+
+# Parse host file(s)
+
+Write-Output '--> Parsing host files'
+
+$hosts             = Parse-Hosts -hosts $hosts
+
+# Output host count prior to removals
+
+Write-Output "--> Valid Hosts Detected: $($hosts.count)"
+
+# Update Regex Removals
+
+Update-Regex-Removals -whitelist $whitelist -wildcards $wildcards -out_file $local_regex
+
+# Fetch Regex criteria
+
+$regex_removals    = (Get-Content $local_regex) -split '\n' | Where {$_}
+
+# Run regex removals
+
+Write-Output "--> Running regex removals"
+
+$hosts             = Regex-Remove -local_regex $regex_removals -hosts $hosts
+
+Write-Output "--> Hosts Detected: $($hosts.count)"
+
+# If check heartbeats is enabled
+
+if($check_heartbeat)
+{
+    Write-Output "--> Checking for heartbeats" 
+    
+    # Check the heartbeats
+
+    Check-Heartbeat -hosts $hosts -out_file $local_nxhosts
+
+}
+
+# Fetch NXHOSTS before finalising
+
+$nxhosts       = (Get-Content $local_nxhosts) -split "\n" | Where {$_}
+
+# Finalise the hosts
+
+Write-Output "--> Finalising"
+
+$hosts             = Finalise-Hosts -hosts $hosts -wildcards $wildcards -nxhosts $nxhosts
+
+Write-Output "--> Hosts added: $($hosts.count)"
+
+# Save host file
+
+Write-Output "--> Saving host file to: $out_file"
+
+Save-Hosts -hosts $hosts -out_file $out_file
---
+++ generator/includes/scripts/functions.ps1
@@ -0,0 +1,306 @@
+﻿Function Fetch-Hosts
+{
+    Param
+    (
+        $w_host_files,
+        $l_host_files,
+        [Parameter(Mandatory=$true)]
+        $dir
+    )
+
+    # SSL Support
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    
+    # If the host download directory exists, clear it out.
+    # Otherwise create a fresh directory
+    if(Test-Path $dir)
+    {
+        Remove-Item "$dir\*" -Recurse
+    }
+    else
+    {
+        New-Item -ItemType Directory -Path $dir
+    }
+
+    # Set Iterator
+    $i = 1
+    
+    # WEB
+    if($w_host_files)
+    {    
+        foreach($host_file in $w_host_files)
+        {
+            # Define host file name
+            $dwn_host = "$dir\$i.txt"
+            
+            # Download the host file
+            (New-Object System.Net.Webclient).DownloadFile($host_file, $dwn_host)
+
+            # Read it
+            $WHL = (Get-Content $dwn_host) -split "\n" | Where {$_}
+
+            # Add hosts to array
+            $hosts += $WHL
+            
+            $i++
+        }
+
+        # Purge downloaded hosts
+        Remove-Item "$dir" -Recurse
+    }
+
+    # LOCAL
+    if($l_host_files)
+    {
+        foreach($host_file in $l_host_files)
+        {
+           # Read it
+           $LHL = (Get-Content $host_file) -split '\n' | Where {$_}
+
+           # Add non-wildcard hosts to  array
+           $hosts += $LHL
+        }
+    }
+
+    return $hosts
+}
+
+Function Parse-Hosts
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        $hosts
+    )
+
+    # Remove local end-zone
+    $hosts        = $hosts -replace '127.0.0.1'`
+                           -replace '0.0.0.0'
+    # Remove whitespace
+    $hosts        = $hosts -replace '\s'
+
+    # Remove user comments
+    $hosts        = $hosts -replace '(#.*)|((\s)+#.*)'
+
+    # Remove www prefixes
+    $hosts        = $hosts -replace '^(www)([0-9]{0,3})?(\.)'
+
+    # Only select 'valid' URLs
+    $hosts        = $hosts | Select-String '(?sim)(localhost)' -NotMatch `
+                           | Select-String '(?sim)(?=^.{4,253}$)(^((?!-)[a-z0-9-]{1,63}(?<!-)\.)+[a-z]{2,63}$)|^([\*])([A-Z0-9-_.]+)$|^([A-Z0-9-_.]+)([\*])$|^([\*])([A-Z0-9-_.]+)([\*])$' -AllMatches
+
+    # Remove empty lines 
+    $hosts        = $hosts | Select-String '(^\s*$)' -NotMatch
+
+    # Remove MatchInfo before selecting unique hosts
+    $hosts        = $hosts -replace ''
+    
+    # Remove duplicates and force lower case
+    ($hosts).toLower() | Sort-Object -Unique
+   
+}
+
+Function Update-Regex-Removals
+{
+
+    Param
+    (
+        $whitelist,
+        $wildcards,
+        [Parameter(Mandatory=$true)]
+        $out_file
+    )
+
+    # Create array
+    $updated_regex_arr = @()
+
+
+    # If there are whitelisted items
+    if($whitelist)
+    {
+        # For each one, format it as regex and add to the array
+        foreach($wl_host in $whitelist)
+        {
+        
+            $wl_host          = $wl_host -replace "\.", "\."
+
+            $wl_host_prefix   = "(?sim)^("
+            $wl_host_suffix   = ")$"
+
+            $wl_host          = $wl_host_prefix + $wl_host + $wl_host_suffix
+
+            $updated_regex_arr += $wl_host
+        
+        }
+    }
+
+    # If there are wildcards passed for removal
+    if($wildcards)
+    {
+        # For each one, format it as regex and add to the array
+
+        foreach($wildcard in $wildcards)
+        {
+        
+            $wildcard          = $wildcard -replace "\.", "\." `
+                                           -replace "\*", "(.*)"
+
+            $wildcard_prefix   = "(?sim)^("
+            $wildcard_suffix   = ")$"
+
+            $wildcard          = $wildcard_prefix + $wildcard + $wildcard_suffix
+
+            $updated_regex_arr += $wildcard
+        
+        }
+    }
+
+    # If the regex removal array has been populated, we need to output it
+    if($updated_regex_arr)
+    {
+        # Sort array and remove duplicates
+        $updated_regex_arr = ($updated_regex_arr).ToLower() | Sort-Object -Unique
+   
+        # Output to file
+        $updated_regex_arr     = $updated_regex_arr -join "`n"
+
+        [System.IO.File]::WriteAllText($out_file,$updated_regex_arr)
+    }
+
+}
+
+Function Regex-Remove
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        $local_regex,
+        [Parameter(Mandatory=$true)]
+        $hosts
+    )
+
+    # Loop through each regex and select only non-matching items
+    foreach($regex in $local_regex)
+    {   
+            $hosts    = $hosts | Select-String $regex -NotMatch
+    }
+
+    # Remove MatchInfo after regex removals
+    $hosts        = $hosts -replace ''
+
+    return $hosts
+
+}
+
+Function Check-Heartbeat
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        $hosts,
+        [Parameter(Mandatory=$true)]
+        $out_file
+    )
+        
+    # Create empty array for NX hosts
+    $nx_hosts     = @()
+
+    # NXDOMAIN native error code
+    $nx_err_code  = 9003
+
+    # Iterator starting point
+    $i            = 1
+    $nx           = 1
+
+    # For each host
+    foreach($nx_check in $hosts)
+    {
+        # Output the current progress
+        Write-Progress -Activity "Querying Hosts" -status "Query $i of $($hosts.Count)" -percentComplete ($i / $hosts.count*100)
+    
+        # Try to resolve a DNS name
+        try
+        {
+            Resolve-DnsName $nx_check -Type A -Server 1.1.1.1 -DnsOnly -QuickTimeout -ErrorAction Stop | Out-Null
+        }
+        # On error
+        catch
+        {
+            # Store error code
+            $err_code = $Error[0].Exception.NativeErrorCode
+
+            # If error code matches NXDOMAIN error code
+            if($err_code -eq $nx_err_code)
+            {
+                # Let the user know
+                Write-Output "--> NXDOMAIN (#$nx): $nx_check"
+            
+                # Add to array
+                $nx_hosts += $nx_check
+
+                # Iterate
+                $nx++
+            }
+        }
+    
+        # Iterate
+        $i++
+    }
+
+    # Remove progress bar
+    Write-Progress -Completed
+
+    # Join array on a new line
+    $nx_hosts = $nx_hosts -join "`n"
+
+    # Output the file
+    [System.IO.File]::WriteAllText($out_file,$nx_hosts)
+}
+
+Function Finalise-Hosts
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        $hosts,
+        $wildcards,
+        $nxhosts
+    )
+
+    # If NXDOMAINS have been specified
+    if($nxhosts)
+    {    
+        # Exclude NXDOMAINS
+        $hosts    = $hosts | Where {$nxhosts -notcontains $_}
+    }
+
+    # Add wildcards to the array after removals
+    $hosts        = $hosts += $wildcards
+
+    # Remove duplicates and force lower case
+    ($hosts).toLower() | Sort-Object -Unique
+
+}
+
+Function Save-Hosts
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        $hosts,
+        [Parameter(Mandatory=$true)]
+        $out_file
+    )
+    
+    # Join on the new lines
+
+    $hosts     = $hosts -join "`n"
+
+    # Add blank line to the end of the host file
+
+    $hosts += "`n"
+
+    # Output to file
+
+    [System.IO.File]::WriteAllText($out_file,$hosts)
+}
